f22c7464ec6eaebb2a66d1e613c173a743b424bb
Sofvi <laitinen.suvi99@gmail.com>
2023-02-26 16:57:17 +0200

    dummy topnav with icons

--- views/Home.js
+++ views/Home.js
@@ -2,18 +2,33 @@ import PropTypes from 'prop-types';
 import List from '../components/List';
 import {
   Avatar,
+  Button,
   Divider,
+  Icon,
   Layout,
   Text,
   TopNavigation,
+  TopNavigationAction,
 } from '@ui-kitten/components';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {StyleSheet, View} from 'react-native';
 
+const MenuIcon = (props) => <Icon {...props} name="menu-outline" />;
+const PostIcon = (props) => <Icon {...props} name="plus-square-outline" />;
+
+const MenuAction = () => <TopNavigationAction icon={MenuIcon} />;
+const PostAction = () => <TopNavigationAction icon={PostIcon} />;
+
 const Home = ({navigation}) => {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <TopNavigation title={renderLogo} alignment="center"></TopNavigation>
+      <TopNavigation
+        accessoryLeft={PostAction}
+        accessoryRight={MenuAction}
+        title={renderLogo}
+        alignment="center"
+      >
+      </TopNavigation>
       <Divider />
       <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <List navigation={navigation} />
